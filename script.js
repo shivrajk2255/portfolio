e8915d3fb404d4c719f88662f30df05038ee27df
Shivraj Kumar <shivrajkumar2255@gmail.com>
2025-05-22 15:08:50 +0530

    Initial project commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
